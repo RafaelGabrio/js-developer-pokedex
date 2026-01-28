908e2abae3a9c83c21c553a5a32e5e382f9047a5
Rafael Gábrio <rafael.lyra@tnpromotora.com.br>
2026-01-28 14:36:09 -0300

    Adição página de detalhes do pokemon

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,35 +1,56 @@
-
-const pokeApi = {}
+const pokeApi = {};
 
 function convertPokeApiDetailToPokemon(pokeDetail) {
-    const pokemon = new Pokemon()
-    pokemon.number = pokeDetail.id
-    pokemon.name = pokeDetail.name
+  const pokemon = new Pokemon();
+  pokemon.number = pokeDetail.id;
+  pokemon.name = pokeDetail.name;
 
-    const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name)
-    const [type] = types
+  const types = pokeDetail.types.map((typeSlot) => typeSlot.type.name);
+  const [type] = types;
 
-    pokemon.types = types
-    pokemon.type = type
+  pokemon.types = types;
+  pokemon.type = type;
 
-    pokemon.photo = pokeDetail.sprites.other.dream_world.front_default
+  pokemon.photo = pokeDetail.sprites.other.dream_world.front_default;
 
-    return pokemon
+  pokemon.abilities = pokeDetail.abilities.map(
+    (abilities) => abilities.ability.name,
+  );
+
+  pokemon.species = pokeDetail.species.name;
+
+  pokemon.height = pokeDetail.height;
+  pokemon.weight = pokeDetail.weight;
+
+  pokemon.states = pokeDetail.stats.map((statSelected) => ({
+    name: statSelected.stat.name,
+    value: statSelected.base_stat,
+  }));
+
+  return pokemon;
 }
 
 pokeApi.getPokemonDetail = (pokemon) => {
-    return fetch(pokemon.url)
-        .then((response) => response.json())
-        .then(convertPokeApiDetailToPokemon)
-}
+  return fetch(pokemon.url)
+    .then((response) => response.json())
+    .then(convertPokeApiDetailToPokemon);
+};
 
 pokeApi.getPokemons = (offset = 0, limit = 5) => {
-    const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`
+  const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
 
-    return fetch(url)
-        .then((response) => response.json())
-        .then((jsonBody) => jsonBody.results)
-        .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
-        .then((detailRequests) => Promise.all(detailRequests))
-        .then((pokemonsDetails) => pokemonsDetails)
-}
+  return fetch(url)
+    .then((response) => response.json())
+    .then((jsonBody) => jsonBody.results)
+    .then((pokemons) => pokemons.map(pokeApi.getPokemonDetail))
+    .then((detailRequests) => Promise.all(detailRequests))
+    .then((pokemonsDetails) => pokemonsDetails);
+};
+
+pokeApi.getPokemonById = (id) => {
+  const url = `https://pokeapi.co/api/v2/pokemon/${id}`;
+
+  return fetch(url)
+    .then((response) => response.json())
+    .catch((error) => console.error(error));
+};
---
+++ assets/js/pokemon-detail.js
@@ -0,0 +1,62 @@
+const urlParams = new URLSearchParams(window.location.search);
+const pokemonId = urlParams.get("id");
+
+const headerPokemonDetail = document.getElementById("headerPokemonDetail");
+const articlePokemonDetail = document.getElementById("articlePokemonDetail");
+const container = document.querySelector("#container");
+const body = document.querySelector("body");
+
+if (pokemonId) {
+  getPokemonDetail(pokemonId).then((pokemon) => {
+    //Adiciona a cor no fundo conforme o tipo
+    // container.classList.add(pokemon.type);
+    body.classList.add(pokemon.type);
+
+    const elementsHeader = convertPokemonDataToItensHeader(pokemon);
+    const elementsArticle = convertPokemonDataToSections(pokemon);
+    headerPokemonDetail.innerHTML += elementsHeader;
+    articlePokemonDetail.innerHTML = elementsArticle;
+  });
+}
+
+function convertPokemonDataToItensHeader(pokemon) {
+  return `
+            <div id="titlePokemon">
+                <h1>${pokemon.name}</h1>
+                <span>#${pokemon.number}</span>
+            </div>
+            
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join("")}
+            </ol>
+
+            <figure>
+                <img src="${pokemon.photo}" alt="img ${pokemon.name}">
+            </figure>
+            
+
+  `;
+}
+
+function convertPokemonDataToSections(pokemon) {
+  return `<section>
+            <h2>About</h2>
+            <dl class="pokemonAttributes">
+                <dt>Species</dt>
+                <dd>${pokemon.species}</dd>
+                <dt>Height</dt>
+                <dd>${pokemon.height}</dd>
+                <dt>Weight</dt>
+                <dd>${pokemon.weight}</dd>
+                <dt>Abilities</dt>
+                <dd>${pokemon.abilities.map((abilities) => abilities).join(", ")}</dd>
+                <dt>States</dt>
+                <dd>${pokemon.states.map((states) => `${states.name}: ${states.value}`).join("</br>")}</dd>
+            </dl>
+          </section>
+  `;
+}
+
+function getPokemonDetail(pokemonId) {
+  return pokeApi.getPokemonById(pokemonId).then(convertPokeApiDetailToPokemon);
+}
